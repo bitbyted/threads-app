010019e1ffa558c3cc1118f5ae4f78d85c06b74a
Nora Di <diminying.dev@gmail.com>
2023-09-17 11:46:10 +1000

    search bug fix

--- components/shared/Searchbar.tsx
+++ components/shared/Searchbar.tsx
@@ -30,13 +30,7 @@ function Searchbar({routeType}: Props) {
   return (
     <div className='searchbar'>
       <Image src='/assets/search-gray.svg' alt='search' width={24} height={24} className='object-contain' />
-      <Input
-        id='text'
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        placeholder={`${routeType !== '/search' ? 'Search communities' : 'Search creators'}`}
-        className='no-focus searchbar_input'
-      />
+      <Input id='text' value={search} onChange={(e) => setSearch(e.target.value)} placeholder='Search...' className='no-focus searchbar_input' />
     </div>
   );
 }
