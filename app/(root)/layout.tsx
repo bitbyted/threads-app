1d32f213eff326c9f36b00a8d33de0b234ad9a6b
Nora Di <diminying.dev@gmail.com>
2023-09-17 11:36:27 +1000

    layout bug fix

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,7 +6,7 @@ import {dark} from '@clerk/themes';
 
 import LeftSidebar from '@/components/shared/LeftSidebar';
 import Bottombar from '@/components/shared/Bottombar';
-import RightSidebar from '@/components/shared/RightSidebar';
+// import RightSidebar from '@/components/shared/RightSidebar';
 import Topbar from '@/components/shared/Topbar';
 
 const inter = Inter({subsets: ['latin']});
@@ -32,7 +32,7 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
               <div className='w-full max-w-4xl'>{children}</div>
             </section>
             {/* @ts-ignore */}
-            <RightSidebar />
+            {/* <RightSidebar /> */}
           </main>
 
           <Bottombar />
